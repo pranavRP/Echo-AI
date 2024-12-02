a4ab6b516f02c5cb4606c675380f42b4923c1e0f
Pranav Pachpande <84848169+pranavRP@users.noreply.github.com>
2024-12-01 22:14:30 -0500

    Update Upload.jsx

--- client/src/component/upload/Upload.jsx
+++ client/src/component/upload/Upload.jsx
@@ -9,7 +9,7 @@ const publicKey = import.meta.env.VITE_IMAGE_KIT_PUBLIC_KEY;
 
 const authenticator = async () => {
   try {
-    const response = await fetch("https://echo-ai-backend.onrender.com/api/upload", {
+    const response = await fetch("http://localhost:3000/api/upload", {
       headers: {
         Authorization: `Bearer ${await window.Clerk.session.getToken()}`,
       },
