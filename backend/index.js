a8457063a59b003993242682baa064a33d61c24a
pranav <pranav.pachpande09@gmail.com>
2025-02-19 17:49:10 -0500

    fixes

--- backend/index.js
+++ backend/index.js
@@ -93,7 +93,7 @@ app.post("/api/chats", ClerkExpressRequireAuth(), async (req, res) => {
   }
 });
 
-app.get("/api/userchats", ClerkExpressRequireAuth(), async (req, res) => {
+app.get("/api/userchats", async (req, res) => {
   const userId = req.auth.userId;
 
   try {
